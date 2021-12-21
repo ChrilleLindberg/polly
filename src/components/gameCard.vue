bfd2c446f31f9b4576275b4ce4b8e24f10e532b7
lukasernsund <lukas.ernsund@gmail.com>
2021-12-21 15:00:10 +0100

    flip cards är nu lit

--- src/components/gameCard.vue
+++ src/components/gameCard.vue
@@ -1,14 +1,14 @@
 <template>
 
-  <transition v-bind:class="{slideLeft: left, slideRight:!left}">
-  <div class="maincontainer" v-show="showCard && !doneCard">
-    <div class="thecard">
-      <div class="thefront"><h1> {{question}} </h1>
-      </div>
-      <div class="theback"><h1> {{answer}}</h1>
+  <transition name="scaleUp">
+    <div class="maincontainer" v-show="showCard && !doneCard">
+      <div v-bind:class="rotateClass" @click="changeClass">
+        <div class="thefront"><h1> {{ question }} </h1>
+        </div>
+        <div class="theback"><h1> {{ answer }}</h1>
+        </div>
       </div>
     </div>
-  </div>
   </transition>
 
 </template>
@@ -20,19 +20,53 @@ export default {
     question: Object,
     answer: Object,
     showCard: Object,
-    doneCard: Object
+    doneCard: Object,
   },
-  /*name: "gameCard",
-  q:"",
-  a:"",
-  show:true*/
+
+  data: function () {
+    return {
+      rotateClass: "frontclass"
+    }
+  },
+  methods: {
+    changeClass: function () {
+      if (this.rotateClass == "thecard2") {
+        this.rotateClass = "thecard"
+        console.log("thecard")
+      }
+      else {
+        this.rotateClass = "thecard2"
+        console.log("thecard2")
+      }
+    }
+  }
+
+
 }
+
+
 </script>
 
 <style scoped>
 
+enter-enlargeIt {
+  animation: enlargeIt 0.5s;
+}
+
+@keyframes enlargeIt {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
 /* THE MAINCONTAINER HOLDS EVERYTHING */
-.maincontainer{
+.maincontainer {
   position: absolute;
   width: 250px;
   height: 320px;
@@ -40,9 +74,11 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  cursor: pointer;
 }
+
 /* THE CARD HOLDS THE FRONT AND BACK FACES */
-.thecard{
+.thecard {
   position: relative;
   top: 0;
   left: 0;
@@ -51,13 +87,25 @@ export default {
   border-radius: 10px;
   transform-style: preserve-3d;
   transition: all 0.8s ease;
+  transform: rotateY(0deg);
 }
-/* THE PSUEDO CLASS CONTROLS THE FLIP ON MOUSEOVER AND MOUSEOUT */
-.thecard:hover{
+
+.thecard2 {
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  transform-style: preserve-3d;
+  transition: all 0.8s ease;
   transform: rotateY(180deg);
 }
+
+
+
 /* THE FRONT FACE OF THE CARD, WHICH SHOWS BY DEFAULT */
-.thefront{
+.thefront {
   position: absolute;
   top: 0;
   left: 0;
@@ -68,9 +116,14 @@ export default {
   overflow: hidden;
   background: #ffc728;
   color: #000;
+  border-radius: 10px;
+  border-color: rgb(0,0,0,50%);
+  border-style: solid;
+  border-width: 0em;
 }
+
 /* THE BACK FACE OF THE CARD, WHICH SHOWS ON MOUSEOVER */
-.theback{
+.theback {
   position: absolute;
   top: 0;
   left: 0;
@@ -84,15 +137,17 @@ export default {
   text-align: center;
   transform: rotateY(180deg);
 }
+
 /*This block (starts here) is merely styling for the flip card, and is NOT an essential part of the flip code */
-.thefront h1, .theback h1{
+.thefront h1, .theback h1 {
   font-family: 'zilla slab', sans-serif;
   padding: 30px;
   font-weight: bold;
   font-size: 24px;
   text-align: center;
 }
-.thefront p, .theback p{
+
+.thefront p, .theback p {
   font-family: 'zilla slab', sans-serif;
   padding: 30px;
   font-weight: normal;
--- src/views/flipCards.vue
+++ src/views/flipCards.vue
@@ -1,47 +1,51 @@
 <template>
 
 
-      <!--<transition name="slideLeft" :css="!swipeRight">-->
+  <!--<transition name="slideLeft" :css="!swipeRight">-->
 
-      <!--<gameCard v-for="card in gameCards" v-bind:key="card">
+  <!--<gameCard v-for="card in gameCards" v-bind:key="card">
 
-      </gameCard>-->
-      <transition-group v-bind:name="transitionType">
-      <gameCard v-for="(item, i) in gameCards.question"
-          v-bind:question="gameCards.question[i]" v-bind:answer="gameCards.answers[i]"
-      :key="item" v-bind:showCard="gameCards.showCard[i]" v-bind:doneCard="gameCards.doneCard[i]">
-      </gameCard>
-      </transition-group>
+  </gameCard>-->
+  <transition-group v-bind:name="transitionType">
+    <gameCard v-for="(item, i) in gameCards.question"
+              v-bind:question="gameCards.question[i]" v-bind:answer="gameCards.answers[i]"
+              :key="item" v-bind:showCard="gameCards.showCard[i]" v-bind:doneCard="gameCards.doneCard[i]">
+    </gameCard>
+  </transition-group>
+
+  <!--</transition>-->
+  <div class="barContainer">
+    <p>
+      Antal kort kvar: {{ gameCards.doneCard.length - gameCards.doneCard.filter(Boolean).length }}
+    </p>
+    <div v-show="gameCards.doneCard.filter(Boolean).length == gameCards.doneCard.length">
+      Du har klarat alla ord!
+    </div>
+
+    <div class="progressBar">
+      <div class="progressBarGreen"
+           v-bind:style="{width: (gameCards.doneCard.filter(Boolean).length / gameCards.doneCard.length)*100 + '%'}">
+      </div>
+    </div>
 
 
-
-
-    <!--</transition>-->
-
-
-    <button @click="swipeLeft">
+  </div>
+  <div class="buttons">
+    <button @click="swipeLeft"
+            v-bind:disabled="gameCards.doneCard.length - gameCards.doneCard.filter(Boolean).length == 1">
       Nej
     </button>
     <button @click="swipeRight">
       JA
     </button>
-
-  <p>
-    Antal avklarade kort: {{gameCards.doneCard.filter(Boolean).length}}
-  </p>
-  <p>
-    Antal kort kvar: {{gameCards.doneCard.length - gameCards.doneCard.filter(Boolean).length}}
-  </p>
-<div v-show="gameCards.doneCard.filter(Boolean).length == gameCards.doneCard.length">
-  Du har klarat alla ord!
-</div>
-
+  </div>
 </template>
 
 <script>
 import gameCard from "../components/gameCard";
 
 import io from 'socket.io-client';
+
 const socket = io();
 
 export default {
@@ -57,20 +61,21 @@ export default {
         question: [""],
         answers: [""],
         showCard: [],
-        doneCard: []},
+        doneCard: []
+      },
       j: Number,
       transitionType: ""
     }
   }, created: function () {
     this.pollId = this.$route.params.id
-    socket.emit("getPollInfo",this.pollId)
+    socket.emit("getPollInfo", this.pollId)
     socket.on('getPollInfo2', (pollInfo) => {
       this.gameCards.question = pollInfo.questions[0].q
       this.gameCards.answers = pollInfo.questions[0].a
-      this.j = this.gameCards.question.length-1;
+      this.j = this.gameCards.question.length - 1;
       this.gameCards.doneCard = new Array(this.gameCards.question.length).fill(false);
       this.gameCards.showCard = new Array(this.gameCards.question.length).fill(false);
-      this.gameCards.showCard[this.gameCards.question.length-1] = true;
+      this.gameCards.showCard[this.gameCards.question.length - 1] = true;
     })
   },
   methods: {
@@ -85,8 +90,8 @@ export default {
       }
       while (this.gameCards.doneCard[this.j] == true) {
         if (this.j == 0) {
-          for (let i=this.gameCards.question.length;i > 0; i--)
-            if(this.gameCards.doneCard[i] == false){
+          for (let i = this.gameCards.question.length; i > 0; i--)
+            if (this.gameCards.doneCard[i] == false) {
               this.j = i + 1;
               break;
             }
@@ -107,12 +112,10 @@ export default {
       if (this.j == -1) {
         this.j = this.gameCards.question.length - 1;
       }
-
-
       while (this.gameCards.doneCard[this.j] == true) {
         if (this.j == 0) {
-          for (let i=this.gameCards.question.length;i > 0; i--)
-            if(this.gameCards.doneCard[i] == false){
+          for (let i = this.gameCards.question.length; i > 0; i--)
+            if (this.gameCards.doneCard[i] == false) {
               this.j = i + 1;
               break;
             }
@@ -120,10 +123,6 @@ export default {
         this.j--;
       }
       this.gameCards.showCard[this.j] = true;
-      console.log("right j end", this.j)
-
-
-
     }
   }
 }
@@ -133,37 +132,59 @@ export default {
 
 <style scoped>
 
+.barContainer {
+  height: 10em;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.progressBar {
+  position: absolute;
+  top: 15%;
+  background-color: red;
+  height: 1em;
+  width: 40%;
+  position: center;
+  vertical-align: center;
+}
+
+.progressBarGreen {
+  height: 100%;
+  background-color: green;
+  transition: width 2s ease-in-out;
+}
+
+.buttons {
+  left:0;
+  width: 100%;
+  position: absolute;
+  bottom: 15%;
+  display: flex;
+  justify-content: center;
+}
+
 /*This block (ends here) is merely styling for the flip card, and is NOT an essential part of the flip code */
 
 
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity .5s;
-}
-
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-
 .slideRight-enter-active,
 .slideRight-leave-active {
-  transition: transform .5s;
+  transition: transform 1s;
 }
 
 .slideRight-enter,
 .slideRight-leave-to {
-  transform: translateY(-50%) translateX(100vw);
+  transform: translateY(-50%) translateX(100vw) rotateZ(45deg);
 }
 
 .slideLeft-enter-active,
 .slideLeft-leave-active {
-  transition: transform .5s;
+  transition: transform 1s;
 }
 
 .slideLeft-enter,
 .slideLeft-leave-to {
-  transform: translateY(-50%) translateX(-100vw);
+  transform: translateY(-50%) translateX(-100vw) rotate(-45deg);
 
 }
 
